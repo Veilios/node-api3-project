2529f15fb9279f43d44402294d549dedaa3936c8
Alan Mir <alanpaulphotos@gmail.com>
2021-03-16 22:26:06 -0400

    Finished ValidateUserId middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -4,8 +4,24 @@ function logger(req, res, next) {
   next();
 };
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  const User = require('../users/users-model');
+
+  const {id} = req.params;
+  const user = await User.getById(id);
+
+  try {
+    if(user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(404).json({ message: "user not found" });
+    };
+  } catch (error) {
+    res.status(500).json({ message: "Error processing request", error: error});
+  };
+  
 }
 
 function validateUser(req, res, next) {
@@ -17,4 +33,4 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
-module.exports = { logger };
+module.exports = { logger, validateUserId, validateUser, validatePost };
